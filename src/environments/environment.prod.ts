b730083ed191e099c068db41a5b9b1ab602e54b8
hasithaMadushan9903 <madushanhasitha4141@gmail.com>
2025-12-31 20:43:39 +0530

    env 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://rapidmanagmentsystembe-production.up.railway.app/api/v1'  // replace with Railway BE domain
+  production: false,
+  apiUrl: 'http://localhost:8080/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api/v1'
+  production: true,
+  apiUrl: 'https://rapidmanagmentsystembe-production.up.railway.app/api/v1'  // replace with Railway BE domain
 };
